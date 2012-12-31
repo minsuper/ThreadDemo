f3705d791a43f998ee53c5f1027a4ee6d349df53
Minsup Kim <minsup@gmail.com>
2012-12-31 09:16:26 -0800

    Thread example for PowerTest

--- src/com/Minsup/ThreadDemo/Thread1.java
+++ src/com/Minsup/ThreadDemo/Thread1.java
@@ -3,23 +3,39 @@ package com.Minsup.ThreadDemo;
 public class Thread1 extends Thread {
 
 	CommonStorage CS;
+	private boolean timerExpired = false;
 	
 	Thread1(CommonStorage CS) {
 		this.CS = CS;
+	}
+	
+	public void TimerExpired() {
+		timerExpired = true;
 	}
 	
 	public void run() {
 		int i;
 		
 		try {
-			for(i=0; i<10000; i++) {
-				//sleep(1);
-				System.out.println(this.getName() + " : " + CS.getData());
+			for(i=0; i<1000; i++) {
+				sleep(2);
+//				System.out.println(this.getName() + " : " + CS.getData());
 				CS.increaseN();
 			}
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
+		}
+		synchronized (this) {
+			try {
+				System.out.println("Before wait");
+				if(timerExpired == false)
+					wait(); 
+				System.out.println("After wait");
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- src/com/Minsup/ThreadDemo/ThreadDemo.java
+++ src/com/Minsup/ThreadDemo/ThreadDemo.java
@@ -9,40 +9,26 @@ public class ThreadDemo {
 		// TODO Auto-generated method stub
 		CommonStorage CS = new CommonStorage();
 		
-		Thread t1 = new Thread1(CS);
-		Thread t2 = new Thread1(CS);
+		Thread1 t1 = new Thread1(CS);
 		
 		try {
 			t1.start();
-			t2.start();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 		try {
-			t1.join();
-			t2.join();
+			Thread.sleep(2000);
+			System.out.println("Timer ended");
+			synchronized (t1) {
+				t1.TimerExpired();
+				t1.notify();
+				System.out.println("notify() called");
+			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
-		}
-		
-		X x = new X();
-		Thread2 t[] = new Thread2[10];
-		for(int i=0; i<10; i++) {
-			t[i] = new Thread2(x);
-			t[i].start();
-		}
-		for(int i=0; i<10; i++) {
-			try {
-				t[i].join();
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("Done");
-		
+		}		
 	}
 
 }
